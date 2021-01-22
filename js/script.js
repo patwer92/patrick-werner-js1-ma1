4d63f44aca71c4120c555ac39a91ee38cc56dc19
Patrick <31260874+patwer92@users.noreply.github.com>
2021-01-23 00:50:49 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,53 @@
+// question 1
+
+const cat = {
+    complain: function() {
+        console.log("Meow!");
+    }
+};
+
+cat.complain();
+
+
+// result: Meow!
+
+
+
+
+// question 2
+
+const heading = document.querySelector("h3");
+
+heading.innerHTML = `Updated Subheading`;
+
+
+
+// question 3
+
+heading.style.fontSize = "2em";
+
+
+
+// question 4
+
+heading.classList.add("Subheading");
+
+
+
+//  question 5
+
+const paragraphs = document.querySelectorAll("p");
+
+for (let i = 0; i < paragraphs.length; i++) {
+    
+    paragraphs[i].style.color = "red";
+}
+
+
+
+
+
+
 const cats = [
     {
         name: "Blob",
@@ -11,4 +61,6 @@ const cats = [
         age: 21
     }
 ];
+
+
 
